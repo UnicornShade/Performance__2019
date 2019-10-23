f55f75701612b47f0d13d6b57a883c479cbabb9e
Dmitry <zadima@gmail.com>
2019-10-24 00:23:00 +0300

    cleanup

--- test.js
+++ test.js
@@ -4,12 +4,4 @@ describe('Page', () => {
       .url('/')
       .assertView('components file list', 'body')
   })
-
-  // it('compare bottom', function () {
-  //   this.browser
-  //     .url('/')
-  //     .scroll(0, 1000)
-  //     .setViewportSize({ width: 1920, height: 3000 })
-  //     .assertView('plain', 'body')
-  // })
 })
